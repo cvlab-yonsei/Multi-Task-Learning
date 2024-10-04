c91275b8e069421f67d542b70fa08e49b98f2464
hw-ch0 <hwc@yonsei.ac.kr>
2024-10-04 12:32:36 +0900

    add description: AMTL

--- LibMTL/weighting/AMTL.py
+++ LibMTL/weighting/AMTL.py
@@ -6,7 +6,12 @@ import numpy as np
 from LibMTL.weighting.abstract_weighting import AbsWeighting
 
 class AMTL(AbsWeighting):
+    r"""Achievement-based Multi-task Learning (AMTL).
+    
+    This method is proposed in `Achievement-based Training Progress Balancing for Multi-Task Learning (ICCV 2023) <https://openaccess.thecvf.com/content/ICCV2023/html/Yun_Achievement-Based_Training_Progress_Balancing_for_Multi-Task_Learning_ICCV_2023_paper.html>`_ \
+    and implemented by us.
 
+    """
     def __init__(self):
         super(AMTL, self).__init__()
         
