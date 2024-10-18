db3559cb2a7070fffcab4a21bb14cd373320eec2
hw-ch0 <hwc@yonsei.ac.kr>
2024-10-18 12:32:49 +0900

    add IMTL_L: loss aggregation part of IMTL

---
+++ LibMTL/weighting/IMTL_L.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+from LibMTL.weighting.abstract_weighting import AbsWeighting
+
+
+class IMTL_L(AbsWeighting):
+
+    def __init__(self):
+        super(IMTL_L, self).__init__()
+    
+    def init_param(self):
+        self.loss_scale = nn.Parameter(torch.tensor([0.0]*self.task_num, device=self.device))
+        
+    def backward(self, losses, **kwargs):
+        losses = self.loss_scale.exp()*losses - self.loss_scale
+        loss = torch.sum(losses)
+        loss.backward()
+        return self.loss_scale.exp().detach().cpu().numpy()
